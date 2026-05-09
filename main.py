c415170f42d7c3655fac9788df83a054995f030d
Callum <callumwilson967@gmail.com>
2026-05-10 00:16:53 +0100

    added separate send and receive topics

--- main.py
+++ main.py
@@ -10,14 +10,15 @@ import cv2
 
 MQTT_BROKER = "broker.hivemq.com"
 MQTT_PORT = 1883
-MQTT_TOPIC = "test/JAgame"
+MQTT_TOPIC = "test/JAgame/fromWebPage"
+MQTT_SEND_TOPIC = "test/JAgame/toWebPage"
 STATE_PATH = "data/state.json"
 
 sense = SenseHat()
 my_stream = BytesIO()
 camera = Picamera2()
-WIDTH = 1024
-HEIGHT = 768
+WIDTH = 650
+HEIGHT = 650
 config = camera.create_preview_configuration({'format': 'YUV420', 'size': (WIDTH, HEIGHT)})
 camera.configure(config)
 camera.start()
@@ -31,26 +32,40 @@ def captureData():
 def on_connect(client, userdata, flags, rc):
     print("MQTT connected with result code", rc)
     client.subscribe(MQTT_TOPIC)
-    print("Subscribed to:", MQTT_TOPIC) 
+    client.subscribe(MQTT_SEND_TOPIC)
+    print("Subscribed to:", MQTT_TOPIC, " and ", MQTT_SEND_TOPIC) 
 
 def on_message(client, userdata, msg):
     print("MQTT message on ", msg.topic)
     payload_str = msg.payload.decode("utf-8")
     data = json.loads(payload_str)
     data["ts"] = int(time.time())
+    data["playerOneScore"] = 0
+    data["playerTwoScore"] = 1
     with open(STATE_PATH, "w") as f:
         json.dump(data, f)
     print("State updated:", payload_str) 
     sense.clear(255,0,0)
     time.sleep(0.5)
     sense.clear()
-    client.publish(MQTT_TOPIC, payload_str)
-    while True:
-        captureData()
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-           break
+    with open(STATE_PATH, "r") as f:
+        stateData = json.load(f)
+
+    stringState = str(stateData)
+    stringState = stringState.replace("'", '"')
+    print(stringState)
+    status = client.publish(MQTT_SEND_TOPIC, stringState) 
+    if status == 0:
+        print(f"Sent Message")
+    else:
+        print(f"Failed to send message")
+
+    #while True:
+    #    captureData()
+    #    if cv2.waitKey(1) & 0xFF == ord('q'):
+    #       break
     # Release the camera and close the windows
-    cv2.destroyAllWindows()
+   # cv2.destroyAllWindows()
 
 def save_state(path=""):
     now = datetime.now()
@@ -64,22 +79,22 @@ def save_state(path=""):
         json.dump(payload, f)
     print("State saved:", payload)
 
-def on_publish(client):
-    with open(STATE_PATH, "r") as file:
-        data = json.load(file)
-
-    status = client.publish(msg.topic, data)
-    if status == 0:
-        print(f"Sent Message")
-    else:
-        print(f"Failed to send message")
+#def on_publish(client):
+#    with open(STATE_PATH, "r") as file:
+#        data = json.load(file)
+#
+#    status = client.publish(MQTT_SEND_TOPIC, data)
+#    if status == 0:
+#        print(f"Sent Message")
+#    else:
+#        print(f"Failed to send message")
 
 
 # Set up MQTT client
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect 
 mqtt_client.on_message = on_message
-mqtt_client.on_publish = on_publish
+#mqtt_client.on_publish = on_publish
 
 mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60) 
 mqtt_client.loop_start()
