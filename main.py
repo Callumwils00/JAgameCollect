c6344cfce2ab7b2e89f79367b1e93a51b6afc08b
Callum <callumwilson967@gmail.com>
2026-05-10 23:00:22 +0100

    changes QOS level

--- main.py
+++ main.py
@@ -22,12 +22,15 @@ HEIGHT = 650
 config = camera.create_preview_configuration({'format': 'YUV420', 'size': (WIDTH, HEIGHT)})
 camera.configure(config)
 camera.start()
+stop_cascade = cv2.CascadeClassifier('stop_data.xml')
 
 def captureData():
     # capture a frame 
     im = camera.capture_array()
+    found = stop_cascade.detectMultiScale(im, minSize=(20,20))
+    for(x,y,w,h) in found:
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 5)
     cv2.imshow("Camera", im)
-
 
 def on_connect(client, userdata, flags, rc):
     print("MQTT connected with result code", rc)
@@ -45,28 +48,22 @@ def on_message(client, userdata, msg):
     with open(STATE_PATH, "w") as f:
         json.dump(data, f)
     print("State updated:", payload_str) 
-    sense.clear(255,0,0)
-    time.sleep(0.5)
+    sense.clear(255,255,255)
+    time.sleep(0.2)
     sense.clear()
+
     with open(STATE_PATH, "r") as f:
         stateData = json.load(f)
-
+    
     stringState = str(stateData)
     stringState = stringState.replace("'", '"')
     print(stringState)
-    status = client.publish(MQTT_SEND_TOPIC, stringState) 
+    status = mqtt_client.publish(MQTT_SEND_TOPIC, stringState, 0) 
     if status == 0:
         print(f"Sent Message")
     else:
         print(f"Failed to send message")
-
-    #while True:
-    #    captureData()
-    #    if cv2.waitKey(1) & 0xFF == ord('q'):
-    #       break
-    # Release the camera and close the windows
-   # cv2.destroyAllWindows()
-
+    
 def save_state(path=""):
     now = datetime.now()
     payload = {
@@ -79,27 +76,48 @@ def save_state(path=""):
         json.dump(payload, f)
     print("State saved:", payload)
 
-#def on_publish(client):
-#    with open(STATE_PATH, "r") as file:
-#        data = json.load(file)
-#
-#    status = client.publish(MQTT_SEND_TOPIC, data)
-#    if status == 0:
-#        print(f"Sent Message")
-#    else:
-#        print(f"Failed to send message")
+def on_publish(client, userdata, msg):
+    with open(STATE_PATH, "r") as file:
+        data = json.load(file)
+    print("publishing message")
+
+    #status = client.publish(MQTT_SEND_TOPIC, data)
+    #if status == 0:
+    #    print(f"Sent Message")
+   # else:
+    #    print(f"Failed to send message")
+#with open(STATE_PATH, "r") as f:
+ #       stateData = json.load(f)
+
+   # stringState = str(stateData)
+  #  stringState = stringState.replace("'", '"')
+   # print(stringState)
+   # status = client.publish(MQTT_SEND_TOPIC, stringState) 
+   # if status == 0:
+   #     print(f"Sent Message")
+   # else:
+   #     print(f"Failed to send message")
 
 
 # Set up MQTT client
 mqtt_client = mqtt.Client()
 mqtt_client.on_connect = on_connect 
 mqtt_client.on_message = on_message
-#mqtt_client.on_publish = on_publish
+mqtt_client.on_publish = on_publish
 
 mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60) 
 mqtt_client.loop_start()
 
+#time.sleep(30)
+
 time.sleep(30)
 
+"""while True:
+    captureData()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+         break
+    # Release the camera and close the windows
+    cv2.destroyAllWindows()
+"""
 mqtt_client.loop_stop()
 mqtt_client.disconnect()
